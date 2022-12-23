365f1334bb6767e348359b41ff10b90567f4eebd
edson2001 <djiedson413@gmail.com>
2022-12-23 16:36:16 +0100

    fix: error puppeteer

--- src/controllers/boot/index.ts
+++ src/controllers/boot/index.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express"
 
+import puppteer from "puppeteer"
+
 export default {
 
     async initBoot(req: Request, res: Response){
+
+        const browser = await  puppteer.launch({
+            headless: true,
+            executablePath: '/chromium/lib'
+        })
+
+        const page = await browser.newPage()
+
+        await page.goto('https://google.com')
 
         return res.json({
             server: 'on'

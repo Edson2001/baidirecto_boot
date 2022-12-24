97d83f0676537622f36a2ec0e453dda81eddc2c6
edson2001 <djiedson413@gmai.com>
2022-12-24 11:03:18 +0100

    fix: boot fix path

--- src/controllers/boot/index.ts
+++ src/controllers/boot/index.ts
@@ -1,24 +1,74 @@
-import { Request, Response } from "express"
+import puppteer from "puppeteer-extra"
+import puppeteerExtraPlugin from "puppeteer-extra-plugin-stealth"
 
-import puppteer from "puppeteer"
+puppteer.use(puppeteerExtraPlugin())
 
-export default {
+async function core(){
+    
+    const browser = await  puppteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-web-security',],
+        ignoreHTTPSErrors: true,  
+        executablePath: process.env.ENV_EXECUTABLEPATH || "C:/Program Files/Google/Chrome/Application/chrome.exe"
+    })
 
-    async initBoot(req: Request, res: Response){
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1280, height: 800 })
+    
+    async function start(){
 
-        const browser = await  puppteer.launch({
-            headless: true,
-            executablePath: '/chromium/lib'
+        console.log("[boot]: boot iniciado")
+
+        await page.goto('https://ib.bancobai.ao', {
+            waitUntil: "networkidle0",
         })
+        
+        await page.waitForSelector("#app > div.scroll-container.no-menu > div.login-wrapper > div.front-content-container > div.container-with-scroll-space > div > div.logo-wrapper > div")
+    }
 
-        const page = await browser.newPage()
+    async function login({user, password}: {user?: string , password?: string}){
+        console.log("[boot]: login iniciado")
+        
+        if(user != '' || password != ''){
+            
+            await page.type("div.input-box.username-input.error-dark-background > input", user!)
+            await page.type("input[type=password]:nth-child(1)",  password!)
+            await page.click("#app > div.scroll-container.no-menu > div.login-wrapper > div.front-content-container > div.container-with-scroll-space > div > button.login-button.login-button") 
+            
+            await page.screenshot({path: "log.png"})
+    
+            const closePopup = await page.waitForSelector("div.popup-modal > div.close-icon", {
+                timeout: 50000
+            })
+            
+            if(closePopup){
+                console.log("[boot]: login success")
+                closePopup.click()
+            } 
 
-        await page.goto('https://google.com')
+        }else{
+            console.log("[boot error]: password and user is empty")
+        }
+    }
 
-        return res.json({
-            server: 'on'
-        })
+    async function getDepositOrder(){
+        console.log("[boot]: get deposit order")
+        const orderDeposit = await page.$eval("#app > div.scroll-container > div.app-content > div.content-container.container.translate-me-top.ready-to-enter > div.table-container.accounts-table > div.simple-table > a:nth-child(2) > div > div:nth-child(4) > span", (text)=> text.innerHTML)
+        return orderDeposit
+    }
+
+    async function stop(){
+        console.log("[boot]: boot stoped")
+        await browser.close()
 
     }
 
-}
+    return {
+        start,
+        login,
+        stop,
+        getDepositOrder
+    }
+} 
+
+export default core
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,26 @@
 import { Router } from "express"
 
-import boot from "../controllers/boot"
+import core from "../controllers/boot"
 
 const route = Router()
 
-route.get('/', boot.initBoot)
+route.get('/', async (req, res)=>{
+    
+    const app = await core()
+
+    await app.start()
+
+    await app.login({
+        user: process.env.ENV_USER_BAI || '',
+        password: process.env.ENV_PASSOWRD_BAI || ''
+    })
+
+    return res.json({
+        bai: {
+            depositOrder: await app.getDepositOrder()
+        }
+    })
+
+})
 
 export default route

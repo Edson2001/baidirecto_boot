f419b4b4d0290316abf0c19bd0affa5594a47362
edson2001 <djiedson413@gmai.com>
2022-12-24 11:23:01 +0100

    fix: test files

--- __tests__/integration/boot.spec.ts
+++ __tests__/integration/boot.spec.ts
@@ -7,10 +7,12 @@ describe("test boot", ()=>{
     
         const app = await core()
         await app.start()
+        
         await app.login({
             user: process.env.ENV_USER_BAI || '',
             password: process.env.ENV_PASSOWRD_BAI || ''
         })
+        
         const deposit = await app.getDepositOrder()
         expect(deposit).toBe(4)
     })
